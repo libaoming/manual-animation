6d7679123bd9d623813b6b148eede11aacf60f32
master <libaoming2@gmail.com>
2016-08-24 10:28:26 +0800

    complete animation manually start and stop use Timer

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -12,26 +12,46 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var image: UIImageView!
     
+    @IBOutlet weak var btn: UIButton!
     var counter = 1
     
-    @IBAction func next(_ sender: AnyObject) {
-      
+    var timer: Timer = Timer()
+    
+    @IBAction func timerManager(_ sender: AnyObject) {
         
-        image.image = UIImage(named: "frame_\(counter)_delay-0.1s.gif")
-        counter += 1
-        
-        if counter == 11 {
-            counter = 0
+        if timer.isValid {
+            timer.invalidate()
+            btn.setTitle("start a animation", for: [])
+        }else {
+            timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(ViewController.next(_:)), userInfo: nil, repeats: true)
+            
+            btn.setTitle("Stop a animation", for: [])
         }
     }
+    @IBAction func next(_ sender: AnyObject) {
+        
+       
+            
+            image.image = UIImage(named: "frame_\(counter)_delay-0.1s.gif")
+            counter += 1
+            
+            if counter == 11 {
+                counter = 0
+            }
+
+    
+      
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+      
+        
+       
     }
 
 

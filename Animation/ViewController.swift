921f624d5eca4e6c163f9ef5ebca8bb2319304d5
master <libaoming2@gmail.com>
2016-08-24 11:05:20 +0800

    complete fadeIn slideIn grow animation use UIview.animation

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
         if timer.isValid {
             timer.invalidate()
             btn.setTitle("start a animation", for: [])
-        }else {
+                   }else {
             timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(ViewController.next(_:)), userInfo: nil, repeats: true)
             
             btn.setTitle("Stop a animation", for: [])
@@ -45,6 +45,33 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func grow(_ sender: AnyObject) {
+        
+        image.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        
+        UIView.animate(withDuration: 1) { 
+            self.image.frame = CGRect(x: 200, y: 200, width: 300, height: 300)
+        }
+        
+    }
+    @IBAction func slideIn(_ sender: AnyObject) {
+        
+        image.center = CGPoint(x: image.center.x - 500, y: image.center.y - 500)
+        
+        UIView.animate(withDuration: 1) { 
+            self.image.center = CGPoint(x: self.image.center.x + 500, y: self.image.center.y + 500)
+        }
+    }
+    @IBAction func fadeIn(_ sender: AnyObject) {
+        
+        image.alpha = 0
+        
+        UIView.animate(withDuration: 1) { 
+        
+            self.image.alpha = 1
+        }
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
